e5571d049ba9c7c1ae86ef36978cd27bc85174ae
zhuXian2902 <2902sandeepkumar@gmail.com>
2020-08-30 12:33:16 +0530

    changes in sub 3

--- src/authController.js
+++ src/authController.js
@@ -3,7 +3,7 @@
 exports.range2 = (req, res, next) => {
 	const { num1, num2 } = req.body;
 	if (num1 < 1000000 && num2 < 1000000) {
-		next();
+		return next();
 	}
 	if (num1 < 1000000 || num2 < 1000000) {
 		return res.json({
