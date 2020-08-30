d85d7664946593cf72a02f79862e52e94b345b78
zhuXian2902 <2902sandeepkumar@gmail.com>
2020-08-30 12:02:12 +0530

    changes in homepage

--- src/calRoute.js
+++ src/calRoute.js
@@ -6,8 +6,7 @@ const { add, sub, mul, div } = require('./calController');
 const { type, range1, range2 } = require('./authController');
 
 router.get('/', (req, res) => {
-	const obj = { message: 'Hello world!' };
-	return res.send(JSON.parse(obj));
+	return res.send('Hello world!');
 });
 router.post('/add', type, range1, add);
 router.post('/sub', type, range2, sub);
