96a9233886bfb9b7afa0798d443c7b9d2a849902
zhuXian2902 <2902sandeepkumar@gmail.com>
2020-08-30 12:44:33 +0530

    changes in /

--- src/calRoute.js
+++ src/calRoute.js
@@ -6,7 +6,8 @@ const { add, sub, mul, div } = require('./calController');
 const { type, range1, range2 } = require('./authController');
 
 router.get('/', (req, res) => {
-	return res.send('Hello world!');
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.end('Hello world!');
 });
 router.post('/add', type, range1, add);
 router.post('/sub', type, range2, sub);
