e3edeb8cacb43c220d6f18499bddf9f18e22bb63
zhuXian2902 <2902sandeepkumar@gmail.com>
2020-08-30 11:39:09 +0530

    changes in add

--- src/authController.js
+++ src/authController.js
@@ -25,7 +25,7 @@ exports.range1 = (req, res, next) => {
 exports.type = (req, res, next) => {
 	const { num1, num2 } = req.body;
 	if (typeof num1 === 'string' || typeof num2 === 'string') {
-		return res.status(400).json({
+		return res.json({
 			status: 'error',
 			message: 'invalid data types',
 		});
