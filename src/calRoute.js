1b1c471bd93c9ccf9bae536d262f0952683293fe
zhuXian2902 <2902sandeepkumar@gmail.com>
2020-09-03 16:04:04 +0530

    new changes again in sub 3

--- src/calRoute.js
+++ src/calRoute.js
@@ -6,8 +6,8 @@ const { add, sub, mul, div } = require('./calController');
 const { type, range1, range2 } = require('./authController');
 
 router.get('/', (req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hello world!');
+	// res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.send('Hello world!');
 });
 router.post('/add', type, range1, add);
 router.post('/sub', type, range2, sub);
